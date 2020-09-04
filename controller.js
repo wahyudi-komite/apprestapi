3e9761f2b0f38dbc46d58f6939acd7a666e698e5
wahyudi-komite <adhye.yudhie@gmail.com>
2020-09-04 19:37:06 +0700

    Success show user by id

--- controller.js
+++ controller.js
@@ -7,8 +7,23 @@ exports.index = function (req, res) {
   response.ok("Application Works", res);
 };
 
-exports.showUsers = function (req, res) {
+exports.showAll = function (req, res) {
   connection.query("SELECT * FROM users", function (error, rows, fields) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
+
+exports.showUsers = function (req, res) {
+  let id = req.params.id;
+  connection.query("SELECT * FROM users WHERE ID =?", [id], function (
+    error,
+    rows,
+    fields
+  ) {
     if (error) {
       connection.log(error);
     } else {
--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ module.exports = function (app) {
   var jsonku = require("./controller");
   app.route("/").get(jsonku.index);
 
-  app.route("/all").get(jsonku.showUsers);
+  app.route("/all").get(jsonku.showAll);
+  app.route("/get/:id").get(jsonku.showUsers);
 };
